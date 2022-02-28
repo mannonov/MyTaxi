e82253eae787fa4efee4713dc75559fbb3bd3490
Jaxadev <jahongir.sh.mannonov@gmail.com>
2022-02-28 21:01:17 +0500

    Home Fragment Ready to use

--- app/src/main/java/uz/behadllc/mytaxi/ui/fragment/HomeFragment.kt
+++ app/src/main/java/uz/behadllc/mytaxi/ui/fragment/HomeFragment.kt
@@ -1,39 +1,188 @@
 package uz.behadllc.mytaxi.ui.fragment
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.content.res.Resources
+import android.location.Location
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
+import android.view.*
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
+import com.google.android.material.navigation.NavigationView
 import uz.behadllc.mytaxi.R
+import uz.behadllc.mytaxi.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), NavigationView.OnNavigationItemSelectedListener {
+
+
+    private val REQUEST_LOCATION_PERMISSION = 712
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var _binding: FragmentHomeBinding
+    private val binding get() = _binding
+    private lateinit var map: GoogleMap
+    private val TAG = "HomeFragment"
+
 
     private val callback = OnMapReadyCallback { googleMap ->
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        map = googleMap
+
+        googleMap.mapType = GoogleMap.MAP_TYPE_TERRAIN
+        googleMap.uiSettings.isMyLocationButtonEnabled = false
+
+
+        setMapStyle(googleMap)
+        enableMyLocation()
+        cameraMoveStartedListener(googleMap)
+
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        _binding = FragmentHomeBinding.inflate(layoutInflater)
+        return _binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val mapFragment = childFragmentManager.findFragmentById(R.id.home_map) as SupportMapFragment?
+        val mapFragment =
+            childFragmentManager.findFragmentById(R.id.home_map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+
+        getCurrentLocation()
+        setUpNavigationView()
+
+        binding.imgNavigationHome.setOnClickListener {
+            getCurrentLocation()
+        }
+
+        binding.homeNavigation.setNavigationItemSelectedListener(this)
+
+        binding.imgHomeMenu.setOnClickListener {
+            binding.homeDrawer.openDrawer(Gravity.LEFT)
+        }
+
+        binding.trips.setOnClickListener {
+
+        }
+
     }
+
+    private fun getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location ->
+                navigateCurrentLocation(location)
+            }
+    }
+
+    private fun setUpNavigationView() {
+
+
+
+    }
+
+    private fun navigateCurrentLocation(location: Location) {
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(location.latitude,
+            location.longitude), 17.0f))
+    }
+
+    private fun cameraMoveStartedListener(googleMap: GoogleMap) {
+        googleMap.setOnCameraMoveStartedListener {
+            binding.mapPoint.playAnimation()
+        }
+        googleMap.setOnCameraMoveCanceledListener {
+            binding.mapPoint.pauseAnimation()
+        }
+    }
+
+    private fun setMapStyle(map: GoogleMap) {
+        try {
+            map.setMapStyle(
+                MapStyleOptions.loadRawResourceStyle(
+                    requireActivity(),
+                    R.raw.map_style
+                )
+            )
+        } catch (e: Resources.NotFoundException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun isPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            requireActivity(),
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun enableMyLocation() {
+        if (isPermissionGranted()) {
+            if (ActivityCompat.checkSelfPermission(
+                    requireActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    requireActivity(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return
+            }
+            map.isMyLocationEnabled = true
+        } else {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_LOCATION_PERMISSION
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray,
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.contains(PackageManager.PERMISSION_GRANTED)) {
+                enableMyLocation()
+            }
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        Log.d("NavigationViewItemClick", "onNavigationItemSelected: $item")
+        return true
+    }
+
+
 }
