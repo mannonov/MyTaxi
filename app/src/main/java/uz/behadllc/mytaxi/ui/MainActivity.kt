4c98236647570658ba04a526226c7f376e94e4ae
Jaxadev <jahongir.sh.mannonov@gmail.com>
2022-02-28 13:26:58 +0500

    initial commit

---
+++ app/src/main/java/uz/behadllc/mytaxi/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package uz.behadllc.mytaxi.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import uz.behadllc.mytaxi.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
